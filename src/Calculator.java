ecc6c5aaabd85a7de3c15d3416da3d4e8ad883a1
JasonPan <jason8399@gmail.com>
2015-05-31 04:01:01 +0800

    finish most Action, and add infix calculate.

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,18 +2,101 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * Created by JasonPan on 5/30/15.
  */
 public class Calculator extends JPanel {
     private JTextField textField;
+    private String buffer;
+    private double memory;
 
     Calculator(){
+        this.buffer = "";
         this.setLayout(new GridLayout(0, 1, 0 ,0));
         this.add(textField = new JTextField());
         this.add(new C_CE_BS_Panel());
         this.add(new Control_Panel());
+    }
+
+    private String getFomulaWithoutBuffer(){
+        String text = textField.getText();
+        return text.substring(0 ,text.length() - buffer.length());
+    }
+
+    private void calculateFormula(){
+        String formula = textField.getText();
+        ArrayList<String> input = new ArrayList<>(Arrays.asList(formula.split(" ")));
+        Stack<String> operand = new Stack<>();
+        Stack<String> operator = new Stack<>();
+        String OPEARTOR = "+-*/";
+        double right, left, ans;
+        int optor;
+
+        //Infix calculate
+        for(String now: input){
+            switch (now){
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    if(!operator.isEmpty()) {
+                        if (OPEARTOR.indexOf(operator.peek()) > OPEARTOR.indexOf(now)) {
+                            right = Integer.parseInt(operand.pop());
+                            left = Integer.parseInt(operand.pop());
+                            optor = OPEARTOR.indexOf(operator.pop());
+                            ans = 0;
+                            switch (optor) {
+                                case 0:
+                                    ans = right + left;
+                                    break;
+                                case 1:
+                                    ans = right - left;
+                                    break;
+                                case 2:
+                                    ans = right * left;
+                                    break;
+                                case 3:
+                                    ans = right / left;
+                                    break;
+                            }
+                            operand.push(Double.toString(ans));
+                        }
+                    }
+                    operator.push(now);
+                    break;
+                default:
+                    operand.push(now);
+                    break;
+            }
+        }
+        for(;!operator.isEmpty();){
+            right = Double.parseDouble(operand.pop());
+            left = Double.parseDouble(operand.pop());
+            optor = OPEARTOR.indexOf(operator.pop());
+            ans = 0;
+            switch (optor) {
+                case 0:
+                    ans = left + right;
+                    break;
+                case 1:
+                    ans = left - right;
+                    break;
+                case 2:
+                    ans = left * right;
+                    break;
+                case 3:
+                    ans = left / right;
+                    break;
+            }
+            operand.push(Double.toString(ans));
+        }
+
+        //Set Ans to Cell
+        textField.setText(buffer = operand.pop());
     }
 
     class C_CE_BS_Panel extends JPanel{
@@ -26,13 +109,17 @@ public class Calculator extends JPanel {
             this.add(back = new JButton("Back"));
             this.add(clearEntry = new JButton("CE"));
             this.add(clear = new JButton("C"));
+            this.back.addActionListener(new Back_ActionListener());
+            this.clear.addActionListener(new C_ActionListener());
+            this.clearEntry.addActionListener(new CE_ActionListener());
         }
 
         class C_ActionListener implements ActionListener{
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                
+                textField.setText("");
+                buffer = "";
             }
         }
 
@@ -40,7 +127,15 @@ public class Calculator extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                String text  = getFomulaWithoutBuffer();
+                if(!text.isEmpty()) {
+                    if (buffer.isEmpty()) {
+                        textField.setText(text.substring(0, text.length() - 3));
+                    } else {
+                        textField.setText(text);
+                        buffer = "";
+                    }
+                }
             }
         }
 
@@ -48,7 +143,12 @@ public class Calculator extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                String text  = textField.getText();
+                if(!text.isEmpty()){
+                    textField.setText(text.substring(0, text.length() - 1));
+                    if(!buffer.isEmpty())
+                        buffer = buffer.substring(0, buffer.length() - 1);
+                }
             }
         }
     }
@@ -68,7 +168,7 @@ public class Calculator extends JPanel {
         private JButton Button7;
         private JButton Button8;
         private JButton Button9;
-        private JButton add;
+        private JButton addition;
         private JButton sub;
         private JButton mul;
         private JButton div;
@@ -103,89 +203,42 @@ public class Calculator extends JPanel {
             this.add(Button0 = new JButton("0"));
             this.add(neg = new JButton("+/-"));
             this.add(point = new JButton("."));
-            this.add(add = new JButton("+"));
+            this.add(addition = new JButton("+"));
             this.add(eq = new JButton("="));
+            this.Button0.addActionListener(new Button_ActionListener());
+            this.Button1.addActionListener(new Button_ActionListener());
+            this.Button2.addActionListener(new Button_ActionListener());
+            this.Button3.addActionListener(new Button_ActionListener());
+            this.Button4.addActionListener(new Button_ActionListener());
+            this.Button5.addActionListener(new Button_ActionListener());
+            this.Button6.addActionListener(new Button_ActionListener());
+            this.Button7.addActionListener(new Button_ActionListener());
+            this.Button8.addActionListener(new Button_ActionListener());
+            this.Button9.addActionListener(new Button_ActionListener());
+            this.addition.addActionListener(new four_fund_ActionListener());
+            this.mul.addActionListener(new four_fund_ActionListener());
+            this.sub.addActionListener(new four_fund_ActionListener());
+            this.div.addActionListener(new four_fund_ActionListener());
+            this.point.addActionListener(new Point_ActionListener());
+            this.neg.addActionListener(new Neg_ActionListener());
+            this.percent.addActionListener(new Percent_ActionListener());
+            this.sqroot.addActionListener((new Sqroot_ActionListener()));
+            this.mulinv.addActionListener(new Mulinv_ActionListener());
+            this.MC.addActionListener(new MC_ActionListener());
+            this.MAdd.addActionListener(new MAdd_ActionListener());
+            this.MR.addActionListener(new MR_ActionListener());
+            this.MS.addActionListener(new MS_ActionListener());
+            this.eq.addActionListener(new Eq_ActionListener());
         }
 
 
 
-        class Button0_ActionListener implements ActionListener {
+        class Button_ActionListener implements ActionListener {
 
             @Override
             public void actionPerformed(ActionEvent e){
-
-            }
-        }
-
-        class Button1_ActionListener implements ActionListener {
-
-            @Override
-            public void actionPerformed(ActionEvent e){
-
-            }
-        }
-
-        class Button2_ActionListener implements ActionListener {
-
-            @Override
-            public void actionPerformed(ActionEvent e){
-
-            }
-        }
-
-        class Button3_ActionListener implements ActionListener {
-
-            @Override
-            public void actionPerformed(ActionEvent e){
-
-            }
-        }
-
-        class Button4_ActionListener implements ActionListener {
-
-            @Override
-            public void actionPerformed(ActionEvent e){
-
-            }
-        }
-
-        class Button5_ActionListener implements ActionListener {
-
-            @Override
-            public void actionPerformed(ActionEvent e){
-
-            }
-        }
-
-        class Button6_ActionListener implements ActionListener {
-
-            @Override
-            public void actionPerformed(ActionEvent e){
-
-            }
-        }
-
-        class Button7_ActionListener implements ActionListener {
-
-            @Override
-            public void actionPerformed(ActionEvent e){
-
-            }
-        }
-
-        class Button8_ActionListener implements ActionListener {
-
-            @Override
-            public void actionPerformed(ActionEvent e){
-
-            }
-        }
-
-        class Button9_ActionListener implements ActionListener {
-
-            @Override
-            public void actionPerformed(ActionEvent e){
-
+                textField.setText(textField.getText() + e.getActionCommand());
+                buffer += e.getActionCommand();
             }
         }
 
@@ -193,7 +246,7 @@ public class Calculator extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e){
-
+                memory = 0;
             }
         }
 
@@ -201,7 +254,10 @@ public class Calculator extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e){
-
+                String text = textField.getText();
+                if(text.isEmpty() || !Character.isDigit(text.charAt(text.length() - 1))){
+                    textField.setText(textField.getText() + memory);
+                }
             }
         }
 
@@ -209,7 +265,7 @@ public class Calculator extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e){
-
+                memory = Double.parseDouble(buffer);
             }
         }
 
@@ -217,39 +273,21 @@ public class Calculator extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e){
-
+                memory += Double.parseDouble(buffer);
             }
         }
 
-        class Add_ActionListener implements ActionListener {
+        class four_fund_ActionListener implements ActionListener {
 
             @Override
             public void actionPerformed(ActionEvent e){
-
-            }
-        }
-
-        class Sub_ActionListener implements ActionListener {
-
-            @Override
-            public void actionPerformed(ActionEvent e){
-
-            }
-        }
-
-        class Mul_ActionListener implements ActionListener {
-
-            @Override
-            public void actionPerformed(ActionEvent e){
-
-            }
-        }
-
-        class Div_ActionListener implements ActionListener {
-
-            @Override
-            public void actionPerformed(ActionEvent e){
-
+                String text = textField.getText();
+                if(!text.isEmpty() && Character.isDigit(text.charAt(text.length() - 1))){
+                    textField.setText(text + " " + e.getActionCommand() + " ");
+                }else{
+                    textField.setText(text.substring(0, text.length() - 3) + " " + e.getActionCommand() + " ");
+                }
+                buffer = "";
             }
         }
 
@@ -257,7 +295,20 @@ public class Calculator extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e){
-
+                String text = textField.getText();
+                double temp;
+                if(!text.isEmpty() && Character.isDigit(text.charAt(text.length() - 1))) {
+                    text = getFomulaWithoutBuffer();
+                    if(buffer.charAt(0) == '-'){
+                        //ERROR
+                    } else {
+                        temp = Double.parseDouble(buffer);
+                        temp = Math.sqrt(temp);
+                        buffer = String.format("%.2f", temp) ;
+                    }
+                    text += buffer;
+                    textField.setText(text);
+                }
             }
         }
 
@@ -265,7 +316,20 @@ public class Calculator extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e){
-
+                String text = textField.getText();
+                double temp;
+                if(!text.isEmpty() && Character.isDigit(text.charAt(text.length() - 1))) {
+                    text = getFomulaWithoutBuffer();
+                    if(buffer.charAt(0) == '-'){
+                        //ERROR
+                    } else {
+                        temp = Double.parseDouble(buffer);
+                        temp *= 0.01;
+                        buffer = String.format("%.2f", temp) ;
+                    }
+                    text += buffer;
+                    textField.setText(text);
+                }
             }
         }
 
@@ -273,7 +337,16 @@ public class Calculator extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e){
-
+                String text = textField.getText();
+                double temp;
+                if(!text.isEmpty() && Character.isDigit(text.charAt(text.length() - 1))) {
+                    text = getFomulaWithoutBuffer();
+                    temp = Double.parseDouble(buffer);
+                    temp = 1 / temp;
+                    buffer = String.format("%.2f", temp) ;
+                    text += buffer;
+                    textField.setText(text);
+                }
             }
         }
 
@@ -281,7 +354,17 @@ public class Calculator extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e){
-
+                String text = textField.getText();
+                if(!text.isEmpty() && Character.isDigit(text.charAt(text.length() - 1))) {
+                    text = getFomulaWithoutBuffer();
+                    if(buffer.charAt(0) == '-'){
+                        buffer = buffer.substring(1);
+                    } else {
+                        buffer = "-" + buffer;
+                    }
+                    text += buffer;
+                    textField.setText(text);
+                }
             }
         }
 
@@ -289,7 +372,10 @@ public class Calculator extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e){
-
+                String text = textField.getText();
+                if(!text.isEmpty() && Character.isDigit(text.charAt(text.length() - 1))){
+                    textField.setText(textField.getText() + e.getActionCommand());
+                }
             }
         }
 
@@ -297,7 +383,7 @@ public class Calculator extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e){
-
+                calculateFormula();
             }
         }
     }

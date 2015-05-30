591fec836d9c40083fc9a175fe1957b1549e802a
JasonPan <jason8399@gmail.com>
2015-05-31 00:10:33 +0800

    add Calculator.java & CalculatorApplet.java

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by JasonPan on 5/30/15.
+ */
+public class Calculator extends JPanel {
+
+    Calculator(){
+
+    }
+}
---
+++ src/CalculatorApplet.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by JasonPan on 5/30/15.
+ */
+public class CalculatorApplet extends JApplet {
+    @Override
+    public void init() {
+        super.init();
+    }
+}
